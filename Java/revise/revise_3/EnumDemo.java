ac1db9d581bf67e160a998185a95b61e806f017b
Jagatheshwaran N <jagatheshwaran.n29@gmail.com>
2026-05-05 05:04:21 -0500

    Added Git Ignore

--- Java/revise/revise_3/EnumDemo.java
+++
@@ -1,9 +0,0 @@
-package preparation.revise_3;
-
-public class EnumDemo {
-
-    public static void main(String[] args) {
-        System.out.println(Status.ACTIVE.getDescription());
-        System.out.println(Status.INACTIVE.getDescription());
-    }
-}
